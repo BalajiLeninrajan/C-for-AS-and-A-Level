fdc08ebff0d9e14de0128a3e422bc2b6afb48fd6
BalajiLeninrajan <bleninrajan@gmail.com>
2023-03-21 21:19:04 +0700

    added calculator.c

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include "helpers.h";
+
+int main(void)
+{
+    // Getting what the user wants to do
+    char operation;
+    outputns("Enter operation ( + | - | / | * | !): ");
+
+    // Converts String to Char (Don't question it, it will make sense later)
+    operation = input()[0];
+
+    // Write code to so that
+    // A. The user can provide the numbers they want to operate on
+    // B. The operation is processed
+    // C. The result is shown
+    // For division on show only the integer part, maybe the insert could help you here
+    // Bonus points for making the code look nice
+    // Bonus points for making the output look nice
+}
